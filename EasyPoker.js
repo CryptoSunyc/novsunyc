d2131eab4135e9e352653660ba3c39a9ca0ecaef
CryptoSunyc <102536966+CryptoSunyc@users.noreply.github.com>
2024-07-13 23:57:48 +0800

    Update EasyPoker.js

--- EasyPoker.js
+++ EasyPoker.js
@@ -1,5 +1,5 @@
 var body = $response.body.replace(/isVip":"\d/g,'isVip":"2')
-.replace(/isVip":0/g,'isVip":2')
+.replace(/mockTotal":0/g,'isVip":10')
 .replace(/vipType":null/g,'vipType":"2"')
 .replace(/singleType":"0"/g,'singleType":null')
 .replace(/vipEndTime":null/g,'vipEndTime":"2025-07-12 15:46:31"')
