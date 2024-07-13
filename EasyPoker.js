abf4df03d9f5366e8324c234ae0223da638394cc
CryptoSunyc <102536966+CryptoSunyc@users.noreply.github.com>
2024-07-14 00:30:20 +0800

    Update EasyPoker.js

--- EasyPoker.js
+++ EasyPoker.js
@@ -1,10 +1,11 @@
-var body = $response.body.replace(/isVip":"0/g,'isVip":"2')
-.replace(/vipType":null/g,'vipType":"2"')
-.replace(/singleType":"0"/g,'singleType":null')
-.replace(/vipEndTime":null/g,'vipEndTime":"2025-07-12 15:46:31"')
-.replace(/"vipInfos":[]/g,'  "vipInfos":[{"setmealId": null,"vipEndTime": "2025-07-12 15:46:31","remainDays": 365,"type": 2,"vipType": 0,"singleType": "0","singleName": "超级会员","ableUpgrade": "1"}]')
-.replace(/"studyGuide":"0"/g,'"studyGuide":"1"')
-.replace(/isVip":0/g,'isVip":2')
-.replace(/"mockTotal":0/g,'"mockTotal":100')
-.replace(/"trainTotal":0/g,'"trainTotal":100')
-$done({ body });
+var obj = JSON.parse($response.body);
+    obj.isVip= "2;
+obj.vipType= "2";
+obj.singleType= null;
+obj.vipEndTime= 31";
+obj.  vipInfos=  "1"}];
+obj.studyGuide= "1";
+obj.isVip= 2;
+obj.mockTotal= 100;
+obj.trainTotal= 100;
+    $done({body: JSON.stringify(obj)});
