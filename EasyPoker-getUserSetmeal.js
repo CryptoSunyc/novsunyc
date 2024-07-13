3640ab5c868ae4d942af0c10e9e497a9ccc9e904
CryptoSunyc <102536966+CryptoSunyc@users.noreply.github.com>
2024-07-14 02:23:53 +0800

    Update and rename EasyPoker-getUserSetmeal to EasyPoker-getUserSetmeal.js

---
+++ EasyPoker-getUserSetmeal.js
@@ -0,0 +1,2 @@
+var body = $response.body.replace(/"vipInfos":[]/g,'  "vipInfos":[{"setmealId": null,"vipEndTime": "2025-07-12 15:46:31","remainDays": 365,"type": 2,"vipType": 0,"singleType": "0","singleName": "超级会员","ableUpgrade": "1"}]')
+$done({ body });
