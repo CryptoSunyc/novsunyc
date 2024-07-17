f617e412af33b65c03f015be0f0d123ac16fb2b7
CryptoSunyc <102536966+CryptoSunyc@users.noreply.github.com>
2024-07-17 19:05:56 +0800

    Update Hi-Fiancevar.js

--- Hi-Fiancevar.js
+++ Hi-Fiancevar.js
@@ -1,3 +1,2 @@
-var body = $response.body.replace(/istrial":"0"/g,'istrial": "1"')
-.replace(/nickname":".*?"/g,'nickname":"已解锁"')
+var body = $response.body.replace(/nickname":".*?"/g,'nickname":"已解锁"')
 $done({ body });
